6acdc20997cb4d7ffae3ba875bab84751095e36f
madhu0699 <76397141+madhu0699@users.noreply.github.com>
2020-12-23 11:10:17 +0530

    Add files via upload

---
+++ StockMarketApi/StockMarket.AccountAPI/Migrations/20201213092943_StockMarketAccount.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StockMarket.AccountAPI.Migrations
+{
+    public partial class StockMarketAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(unicode: false, maxLength: 100, nullable: false),
+                    Password = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    UserType = table.Column<string>(unicode: false, maxLength: 10, nullable: false),
+                    Email = table.Column<string>(unicode: false, maxLength: 40, nullable: false),
+                    MobileNo = table.Column<string>(unicode: false, maxLength: 10, nullable: false),
+                    Confirmed = table.Column<string>(unicode: false, maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
